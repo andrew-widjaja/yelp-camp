ae66fe724b9e642e2073622fe798d84a5bd61a8b
Andrew Widjaja <andrewdwidjaja@gmail.com>
2021-10-07 19:08:25 -0700

    Update flash messages

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,87 +1,107 @@
-const Campground = require('../models/campground');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const Campground = require("../models/campground");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+};
 
 module.exports.renderNewForm = (req, res) => {
-    res.render('campgrounds/new');
-}
+  res.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res, next) => {
-    const geoData = await geocoder.forwardGeocode({
-        query: req.body.campground.location,
-        limit: 1
-    }).send()
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
 
-    const campground = new Campground(req.body.campground);
-    //Add coordinate data of the new campground location to 'campground.geometry'
-    campground.geometry = geoData.body.features[0].geometry;
-    //Make an array that contains the url and filename of the uploaded images and adds them to the campground.images model properties
-    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    //Add user id to campground.author to associate the created campground with the user
-    campground.author = req.user._id
-    await campground.save();
-    console.log(campground)
+  const campground = new Campground(req.body.campground);
+  //Add coordinate data of the new campground location to 'campground.geometry'
+  campground.geometry = geoData.body.features[0].geometry;
+  //Make an array that contains the url and filename of the uploaded images and adds them to the campground.images model properties
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  //Add user id to campground.author to associate the created campground with the user
+  campground.author = req.user._id;
+  await campground.save();
+  console.log(campground);
 
-    //Flash message and redirect to new campground show page
-    req.flash('success', `Successfully created the ${campground.title} campground!`);
-    res.redirect(`/campgrounds/${campground._id}`)
-}
+  //Flash message and redirect to new campground show page
+  req.flash(
+    "success",
+    `Successfully created the ${campground.title} campground!`
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.showCampground = async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        //Nested populate that will populate the reviews, the authors of the reviews, then the author of the campground itself
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    if (!campground) {
-        req.flash('error', 'Sorry, cannot find that campground!');
-        return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/show', { campground });
-}
+  const campground = await Campground.findById(req.params.id)
+    .populate({
+      //Nested populate that will populate the reviews, the authors of the reviews, then the author of the campground itself
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Sorry, could not find that campground...");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
 
 module.exports.renderEditForm = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id)
-    if (!campground) {
-        req.flash('error', 'Sorry, cannot find that campground!');
-        return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/edit', { campground });
-}
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Sorry, could not find that campground...");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
 
 module.exports.updateCampground = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    //This code allows you to upload new images on the edit page
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    campground.images.push(...imgs);
-    await campground.save();
-    //Deleting images from the Edit form
-    if (req.body.deleteImages) {
-        //This for loop will remove the images from the Cloudinary storage
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        //Query to pick out images and removing it from Mongo
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  //This code allows you to upload new images on the edit page
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  await campground.save();
+  //Deleting images from the Edit form
+  if (req.body.deleteImages) {
+    //This for loop will remove the images from the Cloudinary storage
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
     }
-    req.flash('success', `Successfully updated the ${campground.title} campground`);
-    res.redirect(`/campgrounds/${campground._id}`)
-}
+    //Query to pick out images and removing it from Mongo
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
+  req.flash(
+    "success",
+    `Successfully updated the ${campground.title} campground.`
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.deleteCampground = async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    req.flash('success', `Successfully deleted the ${campground.title} campground`);
-    res.redirect('/campgrounds');
-}
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndDelete(id);
+  req.flash(
+    "success",
+    `Successfully deleted the ${campground.title} campground.`
+  );
+  res.redirect("/campgrounds");
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,39 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.renderRegister = (req, res) => {
-    res.render('users/register');
-}
+  res.render("users/register");
+};
 
 module.exports.register = async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to YelpCamp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to YelpCamp!");
+      res.redirect("/campgrounds");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("register");
+  }
+};
 
 module.exports.renderLogin = (req, res) => {
-    res.render('users/login')
-}
+  res.render("users/login");
+};
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'You are now logged in!');
-    //Setting redirectUrl to the req.originalUrl (see middleware.js) OR /campgrounds, then deleting it from the session right after
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-}
+  req.flash("success", "You are now logged in.");
+  //Setting redirectUrl to the req.originalUrl (see middleware.js) OR /campgrounds, then deleting it from the session right after
+  const redirectUrl = req.session.returnTo || "/campgrounds";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl);
+};
 
 module.exports.logout = (req, res) => {
-    req.logout();
-    req.flash('success', 'You have been logged out.')
-    res.redirect('/campgrounds');
-}
+  req.logout();
+  req.flash("success", "You have been logged out.");
+  res.redirect("/campgrounds");
+};
